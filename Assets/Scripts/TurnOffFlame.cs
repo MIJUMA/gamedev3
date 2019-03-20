cca6b7b54ba2ee959f81b3d3387cbbb841f56f9b
MIJUMA <mikael.juhani.manninen@gmail.com>
2019-03-20 20:28:27 +0200

    TurnOffFire added

---
+++ Assets/Scripts/TurnOffFlame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffFlame : MonoBehaviour {
+
+    public Transform flame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        flame.GetComponent<ParticleSystem>().enableEmission = true;
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Cup" || other.gameObject.name == "WaterDrop")
+        {
+            flame.GetComponent<ParticleSystem>().enableEmission = false;
+
+            Debug.Log("Turned off");
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            flame.GetComponent<ParticleSystem>().enableEmission = false;
+        }
+    }
+}
