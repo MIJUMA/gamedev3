78def3107615e67235de286138cf12461e614015
MIJUMA <mikael.juhani.manninen@gmail.com>
2019-04-03 21:52:09 +0300

    Inventory added to Hell scene

---
+++ gamedev3/Assets/MainResources/Scripts/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+        instance = this;
+    }
+
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if (!item.isDefaultItem)
+        {
+            items.Add(item);
+            return true;
+        }
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+            return false;
+        }
+        return false;
+    }
+    public void Remove (Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
+    }
+
+}
---
+++ gamedev3/Assets/MainResources/Scripts/ItemPickUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Item item;
+
+    public void Interact() {
+
+        PickUp();
+    }
+
+     void PickUp ()
+    {
+        Debug.Log("Picking up item." + item.name);
+        bool wasPickedUp = Inventory.instance.Add(item);
+        
+
+        if (wasPickedUp)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
